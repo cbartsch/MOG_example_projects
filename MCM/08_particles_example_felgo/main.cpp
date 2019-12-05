58d6859a668ada31a4f74f3b8a19677a850e6505
Christian Bartsch <chrisu.bartsch@gmail.com>
2019-12-05 15:59:47 +0100

    Update graphics examples to Unity 2019

--- MCM/08_particles_example_felgo/main.cpp
+++ MCM/08_particles_example_felgo/main.cpp
@@ -18,13 +18,13 @@ int main(int argc, char *argv[])
 
   // use this during development
   // for PUBLISHING, use the entry point below
-  felgo.setMainQmlFileName(QStringLiteral("qml/MultiplayerExampleMain.qml"));
+  felgo.setMainQmlFileName(QStringLiteral("qml/Main.qml"));
 
   // use this instead of the above call to avoid deployment of the qml files and compile them into the binary with qt's resource system qrc
   // this is the preferred deployment option for publishing games to the app stores, because then your qml files and js files are protected
   // to avoid deployment of your qml files and images, also comment the DEPLOYMENTFOLDERS command in the .pro file
   // also see the .pro file for more details
-  //  vplay.setMainQmlFileName(QStringLiteral("qrc:/qml/MultiplayerExampleMain.qml"));
+  //  vplay.setMainQmlFileName(QStringLiteral("qrc:/qml/Main.qml"));
 
   engine.load(QUrl(felgo.mainQmlFileName()));
 
