67cb4df450f85a7a9c7d7314ba8f986e5eb782b6
Chrisu <chrisu.bartsch@gmail.com>
2020-11-24 21:25:27 +0100

    Update and improve physics example

--- MC/08_controls_example/Assets/Scripts/Player.cs
+++ MC/08_controls_example/Assets/Scripts/Player.cs
@@ -3,29 +3,23 @@ using System.Collections;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
-
     public float moveSpeed = 100, rotateSpeed = 400;
 
     // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start() {
+    }
 
+    // Update is called once per frame
+    void Update() {
         var body = GetComponent<Rigidbody>();
 
         //use CustomInputManager to control instead of default Input class
-        body.AddForce(transform.forward * moveSpeed *
-            CustomInputManager.GetAxis("Vertical"));
+        body.AddForce(Input.GetAxis("Vertical") * moveSpeed * transform.forward);
 
-        body.AddTorque(transform.up * rotateSpeed *
-            CustomInputManager.GetAxis("Horizontal"));
+        body.AddTorque(Input.GetAxis("Horizontal") * rotateSpeed * transform.up);
 
         //reset player if fallen down
-        if (transform.position.y < -10)
-        {
+        if (transform.position.y < -10) {
             transform.position = Vector3.zero;
             transform.rotation = Quaternion.identity;
 
