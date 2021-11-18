78a3a356a4abd6c3099176aa3c750cc56aae62bc
Chrisu <chrisu.bartsch@gmail.com>
2021-11-18 15:53:26 +0100

    Update 3D models example to starter assets

--- MC/06_3d_models_test/Assets/CharacterController.cs
+++ MC/06_3d_models_test/Assets/CharacterController.cs
@@ -1,7 +1,7 @@
 ﻿ using UnityEngine;
 using System.Collections;
 
-public class CharacterController : MonoBehaviour {
+public class MyCharacterController : MonoBehaviour {
 
     private Animator animator;
     
